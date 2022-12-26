5be7eeb2fcd4ddb3746cb3edf50cef9e0b6329c2
zodman <zodman@gmail.com>
2022-12-26 02:46:51 -0500

    update

--- src/wpcli/__main__.py
+++ src/wpcli/__main__.py
@@ -1,2 +1,14 @@
-from .post import post
-post()
+import click
+from rich import print
+
+
+@click.command()
+def help():
+    print('[bold cyan]For execute: ')
+    print('python -m wpcli.post --help')
+    print('python -m wpcli.tag --help')
+    print('python -m wpcli.cat --help')
+
+
+if __name__ == "__main__":
+    help()
